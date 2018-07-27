afb8285e892aa049439026bed99b46c185f5416b
Louis Lam <llam@pivotal.io>
2018-07-26 22:42:26 -0700

    update

--- pynsxv/library/nsx_lb.py
+++ pynsxv/library/nsx_lb.py
@@ -681,8 +681,14 @@ def add_member(client_session, esg_name, pool_name, member_name, member_ip, port
         pool_details['member'] = []
         members = []
 
-    new_member = {'name': member_name, 'ipAddress': member_ip, 'port': port, 'monitorPort': monitor_port,
+    new_member = {'name': member_name, 'ipAddress': member_ip,
                   'weight': weight, 'maxConn': max_conn, 'minConn': min_conn}
+    if port != None:
+            new_member['port'] = port
+
+    if monitor_port != None:
+            new_member['monitorPort'] = monitor_port
+
 
     members.append(new_member)
     pool_details['member'] = members
